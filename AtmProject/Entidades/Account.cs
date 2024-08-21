0ae30cc429bcdab6dbe7c7a5f73a76df0782a77b
Beater <127983605+beater-cs@users.noreply.github.com>
2024-08-20 23:15:20 -0400

    Criando Entidades

---
+++ AtmProject/Entidades/Account.cs
@@ -0,0 +1,19 @@
+﻿namespace AtmProject.Entidades
+{
+    public class Account
+    {
+        public Account()
+        {
+            this.Balance = 0;
+        }
+        public int AccNum { get; set; }
+        public string? Name { get; set; }
+        public string? Phone { get; set; }
+        public string? Address { get; set; }
+        public string? Education { get; set; }
+        public DateTime Dob { get; set; }
+        public int Pin { get; set; }
+        public decimal Balance { get; set; }
+    }
+
+}
---
+++ AtmProject/Entidades/Transactions.cs
@@ -0,0 +1,12 @@
+﻿namespace AtmProject.Entidades
+{
+    public class Transaction
+    {
+        public int TransactionID { get; set; }
+        public int AccNum { get; set; }
+        public string Type { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime TDate { get; set; }
+    }
+
+}
